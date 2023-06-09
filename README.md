39e52d32c5a2d0e6212c3f1b0e24c8f078707b3a
iea <iea@tutanota.com>
2023-06-09 14:17:46 +0000

    forgot

--- README.md
+++ README.md
@@ -4,6 +4,6 @@ approx 100x50 area
 
 terminal that supports vt100 escape codes
 
-conio.h or an equivalent getch function (unbuffered, non-echoing)
+conio.h or library with an equivalent getch function (unbuffered, non-echoing)
 
 patience
--- klondike.c
+++ klondike.c
@@ -160,7 +160,7 @@ int main(void)
 
         if(wincon == 1)
         {   printf("Game complete.\n");
-            printf("Elapsed time: %.1f minutes", ((double) clock() - start / CLOCKS_PER_SEC) / 60); //still dont know if this works
+            printf("Elapsed time: %.1f minutes", ((double) clock() - start / CLOCKS_PER_SEC * 60));
             int block = getch();
             if(block == '\b' || block == 3) break;
             printf("\033[H\033[2J");
